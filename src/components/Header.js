36399e48dc5a1d34e8341cde2e85fd2c46d7b4b5
John Curry <fastpenguin91@gmail.com>
2021-09-16 17:49:53 -0700

    simple header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import { AppBar, Toolbar, MenuItem } from "@material-ui/core";
-import { useHistory } from "react-router-dom";
 
 function Header() {
-  let history = useHistory();
   return (
     <div>
       <AppBar position="sticky">
         <Toolbar>
-          <MenuItem onClick={() => history.pushState("/")}>Home</MenuItem>
-          <MenuItem onClick={() => history.pushState("/login")}>Login</MenuItem>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Login</MenuItem>
         </Toolbar>
       </AppBar>
     </div>
