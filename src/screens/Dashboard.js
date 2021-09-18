061d5d8727da27b6d199ffbae7c04aa55630d91e
John Curry <fastpenguin91@gmail.com>
2021-09-17 17:41:08 -0700

    error

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,13 +1,39 @@
-import { GET_USERS_QUERY, TODOS_QUERY } from "../graphql/queries";
-import { useQuery } from "@apollo/client";
-import { Container, Box } from "@material-ui/core";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Container, Box, Button, TextField } from "@material-ui/core";
 import Todos from "../components/Todos";
+import { NEW_TODO } from "../graphql/mutations";
 
 function Dashboard() {
-  // const { data, loading, error } = useQuery(GET_USERS_QUERY);
+  const [createTodo, { error }] = useMutation(NEW_TODO);
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createTodo({
+      variables: {
+        newtodo: todoText,
+        userId: "1",
+        isComplete: false,
+      },
+    });
+  };
 
   return (
     <Container>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          label="Add a todo"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+          variant="outlined"
+          margin="normal"
+        />
+        <Button fullWidth type="submit" variant="contained" color="primary">
+          Add todo
+        </Button>
+      </form>
       <Box align="center">
         <Todos />
       </Box>
