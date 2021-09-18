f45f687817f08b4f1361ff4f8a34358abc2c3533
John Curry <fastpenguin91@gmail.com>
2021-09-17 19:42:32 -0700

    done

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -3,9 +3,16 @@ import { useMutation } from "@apollo/client";
 import { Container, Box, Button, TextField } from "@material-ui/core";
 import Todos from "../components/Todos";
 import { NEW_TODO } from "../graphql/mutations";
+import { TODOS_QUERY } from "../graphql/queries";
 
 function Dashboard() {
-  const [createTodo, { error }] = useMutation(NEW_TODO);
+  const [createTodo, { error }] = useMutation(NEW_TODO, {
+    refetchQueries: [
+      {
+        query: TODOS_QUERY,
+      },
+    ],
+  });
   const [todoText, setTodoText] = useState("");
 
   const handleSubmit = (event) => {
