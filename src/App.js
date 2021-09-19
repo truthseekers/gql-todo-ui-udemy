210bbdd466789ad8ce74aab2eea22d6c9f75f3e7
John Curry <fastpenguin91@gmail.com>
2021-09-18 17:09:01 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import Header from "./components/Header";
 import AuthenticatedApp from "./AuthenticatedApp";
+import { Switch, Route } from "react-router-dom";
+import Signup from "./screens/Signup";
+import UnauthenticatedApp from "./screens/UnauthenticatedApp";
 
 function App() {
+  // const { currentuser } = useAuth();
+  const currentUser = false;
+
   return (
     <div>
       <Header />
-      <AuthenticatedApp />
+      {currentUser ? (
+        <AuthenticatedApp />
+      ) : (
+        <Switch>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/">
+            <UnauthenticatedApp />
+          </Route>
+        </Switch>
+      )}
     </div>
   );
 }
---
+++ src/screens/UnauthenticatedApp.js
@@ -0,0 +1,5 @@
+function UnauthenticatedApp() {
+  return <div>This is the homepage for the unauthenticated user</div>;
+}
+
+export default UnauthenticatedApp;
