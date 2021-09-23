f1e387a2ffc8a99abe9efe042f689d4cf8b844c2
John Curry <fastpenguin91@gmail.com>
2021-09-22 17:46:33 -0700

    done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import AuthenticatedApp from "./AuthenticatedApp";
 import { Switch, Route } from "react-router-dom";
 import Signup from "./screens/Signup";
 import UnauthenticatedApp from "./screens/UnauthenticatedApp";
+import Login from "./screens/Login";
 
 function App() {
   // const { currentuser } = useAuth();
-  const currentUser = true;
+  const currentUser = false;
 
   return (
     <div>
@@ -16,6 +17,9 @@ function App() {
         <AuthenticatedApp />
       ) : (
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/signup">
             <Signup />
           </Route>
---
+++ src/screens/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Container,
+  Typography,
+  TextField,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { useLoginStyles } from "../styles/loginStyles";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const classes = useLoginStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //doLogin()
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h3">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
+          <TextField
+            autoFocus
+            fullWidth
+            id="email"
+            label="Email Address"
+            margin="normal"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            value={email}
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            id="password"
+            label="Password"
+            margin="normal"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+            value={password}
+            variant="outlined"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Link to="/signup">{"Don't have an account? Sign up!"}</Link>
+        </form>
+      </div>
+    </Container>
+  );
+}
